1a635ca308d0444fec846cdb35d849adb1ddba5d
Yura <you@example.com>
2023-06-08 21:57:27 +0300

    ХЗ

--- C#Home/Домашка/FirsrCode/Program.cs
+++ C#Home/Домашка/FirsrCode/Program.cs
@@ -1,13 +1,11 @@
-﻿int a = 1;
-int b = 2;
-int c = 6;
-int d = 8;
-int e = 4;
-int max = a;
-if (a>max) max = a;
-if (b>max) max = b;
-if (c>max) max = c;
-if (d>max) max = d;
-if (e>max) max = e;
-Console.WriteLine ("max=");
-Console.WriteLine (max);
+﻿Console.Write ("Привет, как тебя зовут?");
+srting username = Console.ReadLine();
+if(username.ToLower()== Маша);
+{
+Console.WriteLine("Ура, это Маша");
+}
+else 
+{
+Console.WriteLine ("Привет");
+Console.WriteLine (username);
+}
